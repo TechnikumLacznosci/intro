5fc7c01aa0d925e562cb004c0a439ccc5e6b4e74
Konrad Mrozewski <k.mrozewski@kainos.com>
2018-05-16 15:04:57 +0200

    display values in table

--- __init__.py
+++ __init__.py
@@ -10,19 +10,22 @@ window = Tk()
 window.title("ToDo list app")
 
 # Define number of columns
-column_count =
+column_count = len(task_list[0])
 # Define number of rows
-row_count =
+row_count = len(task_list)
 # Empty table object with size row_count x column_count
 table = SimpleTable(window, row_count, column_count)
 table.pack(side='top', fill='x')
 
 # Fill table with values
-table.set(0, 0, task_list[0]['title'])
+for i in range(0, row_count):
+    for j in range(0, column_count):
+        table.set(i, j, get_from_table(i, j, table))
+        #table.set(i, j, task_list[i][task_list[i].keys()[j]])
 
 
-
-
+def get_from_table(i, j, tab):
+    return 0
 
 
 # Run app
