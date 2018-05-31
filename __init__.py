920ddf621689b4885c852c8f6b3a829f4a2c6d30
Damien87 <damiankreft87@gmail.com>
2018-06-01 01:13:56 +0200

    Version with the functionality of getting data from a local file

--- __init__.py
+++ __init__.py
@@ -1,32 +1,54 @@
 from Tkinter import *
 from SimpleTable import *
 from TaskDownloader import Task
-
-# Instantiate window
-task = Task("https://raw.githubusercontent.com/TechnikumLacznosci/intro/master/files/tasks.json")
-task_list = task.get_all()
+import json
+import tkFileDialog
+import urllib2
 
 window = Tk()
 window.title("ToDo list app")
 
-# Define number of columns
-column_count = len(task_list[0])
-# Define number of rows
-row_count = len(task_list)
-# Empty table object with size row_count x column_count
-table = SimpleTable(window, row_count, column_count)
-table.pack(side='top', fill='x')
+json_file_uri = "https://raw.githubusercontent.com/TechnikumLacznosci/intro/master/files/tasks.json"
 
-# Fill table with values
-for i in range(0, row_count):
-    for j in range(0, column_count):
-        table.set(i, j, get_from_table(i, j, table))
-        #table.set(i, j, task_list[i][task_list[i].keys()[j]])
+def instantiate_table(data_list):
+    # Define number of columns
+    column_count = len(data_list[0])
+    # Define number of rows
+    row_count = len(data_list)
+    # Empty table object with size row_count x column_count
+    table = SimpleTable(window, row_count, column_count)
+    table.pack(side='top', fill='x')
+    
+    def get_from_table(i, j, tab):
+        return data_list[i][data_list[i].keys()[j]]
+    
+    # Fill table with values
+    for i in range(0, row_count):
+        for j in range(0, column_count):
+            table.set(i, j, get_from_table(i, j, table))
 
+# Add file dialog button
+def data_call_file_dialog():
+    window.filename = tkFileDialog.askopenfilename(initialdir = 'C:\\',title="Select file: ", filetypes= (("JSON File",'*.json'), ("All files", '*')))
+    with open(window.filename, 'r') as json_file:
+        Data_list = json.load(json_file)
+        instantiate_table(Data_list)
 
-def get_from_table(i, j, tab):
-    return 0
+def data_get_from_internet():
+    try:
+        task = Task(json_file_uri)
+        task_list = task.get_all()
+        instantiate_table(task_list)
+    except urllib2.URLError:
+        print("Failed to load the given URL")
+        
 
+# Add 'Load tasks' button
+button_call_table = Button(window, text="Load tasks", command=data_get_from_internet)
+button_call_table.grid(column='0',row='0')
+
+button_call_file_dialog = Button(window, text="Choose file", command=data_call_file_dialog)
+button_call_file_dialog.grid(column='1',row='0')
 
 # Run app
 window.mainloop()
